cb3fe2cd3bab4e5f018c6d62e9f95aa720642787
TEH KAI KEAT <dtfteh@gmail.com>
2020-10-17 09:45:44 +0800

    Testing Java

--- src/main/java/helloworld.java
+++ src/main/java/helloworld.java
@@ -10,7 +10,7 @@
  */
 public class helloworld {
     public static void main(String[] args) {
-        System.out.println("Testing Testing 1 2 3");
+        System.out.println("Testing Testing 1 2 3 4");
     
 }
 }
